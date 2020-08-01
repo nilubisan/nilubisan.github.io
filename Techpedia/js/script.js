afde2329b812c60bab3f534e020c7de6643db7aa
nilubisan <62594115+nilubisan@users.noreply.github.com>
2020-08-01 17:15:36 +0600

    Add files via upload

---
+++ Techpedia/js/script.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", function () {
+  let listToggler = document.querySelectorAll(".navbar-link-pointer");
+  [].forEach.call(listToggler, function (item) {
+    item.addEventListener("click", function (e) {
+      if (item.innerHTML == "⯆") item.innerHTML = "⯈";
+      else item.innerHTML = "⯆";
+      let sibl = e.target.nextElementSibling;
+      sibl.nextElementSibling.classList.toggle("nested-show");
+    });
+  });
+
+  document.getElementById("burger").addEventListener("click", function (e) {
+    document.querySelector("html").classList.toggle("lock");
+    document.getElementById("navbar").classList.toggle("active");
+    document.getElementById("main-doc").classList.toggle("active");
+    this.classList.toggle("active");
+  });
+
+  let linkList = document.querySelectorAll(".nav-link");
+  [].forEach.call(linkList, function (item) {
+    item.addEventListener("click", function (e) {
+      document.querySelector("html").classList.remove("lock");
+      document.getElementById("navbar").classList.remove("active");
+      document.getElementById("burger").classList.remove("active");
+      document.getElementById("main-doc").classList.remove("active");
+    });
+  });
+});
